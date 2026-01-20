5cbe0524d8ed12e81dae02888673d4052e35598c
aanesn <anesen.niklas@gmail.com>
2026-01-20 03:18:20 +0100

    input fix

--- web/src/lib/components/Input.svelte
+++ web/src/lib/components/Input.svelte
@@ -7,7 +7,7 @@
 
 <input
 	class={cx(
-		"flex h-11 w-full min-w-0 rounded-full bg-neutral-900 px-4.5 py-1 text-base shadow-xs outline-none placeholder:text-neutral-500 md:text-sm",
+		"flex h-11 w-full min-w-0 rounded-full bg-neutral-900 px-4.5 py-1 shadow-xs outline-none placeholder:text-neutral-500",
 		className
 	)}
 	bind:value
