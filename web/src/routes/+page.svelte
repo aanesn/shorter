fe7888998aaaf30337c2d4eddbc03ecf2861d150
aanesn <anesen.niklas@gmail.com>
2026-01-20 02:37:36 +0100

    title desc

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,3 +2,14 @@
 	<title>Shorter</title>
 	<meta name="description" content="A domain shortener tool" />
 </svelte:head>
+
+<div class="py-20 lg:py-40">
+	<div class="flex flex-col items-center gap-y-4">
+		<h1 class="text-center font-manrope text-4xl font-semibold md:text-5xl lg:text-6xl">
+			Search for shorter <br /> domains
+		</h1>
+		<h2 class="max-w-sm text-center text-neutral-400 max-md:text-balance lg:text-lg">
+			Discover shorter versions of your domain like linktree.com -> linktr.ee
+		</h2>
+	</div>
+</div>
