d3fa4cba45d56f4c7891b68aba232db627aca758
aanesn <anesen.niklas@gmail.com>
2026-03-19 20:20:30 +0100

    get domain

--- src/search.rs
+++ src/search.rs
@@ -1,8 +1,9 @@
-use crate::error::AppError;
+use crate::{error::AppError, tlds::TLDS};
 use axum::{extract::Query, response::IntoResponse};
 use serde::Deserialize;
 
 const MAX_QUERY_LEN: usize = 255;
+const DEFAULT_DOMAIN: &str = "com";
 
 #[derive(Deserialize)]
 pub struct SearchParams {
@@ -15,7 +16,8 @@ pub async fn get(
     let q = params.q.trim().to_lowercase();
     check_query(&q)?;
 
-    Ok(params.q.to_owned())
+    let domain = get_domain(&q);
+    Ok(domain)
 }
 
 fn check_query(q: &str) -> anyhow::Result<()> {
@@ -26,4 +28,13 @@ fn check_query(q: &str) -> anyhow::Result<()> {
         anyhow::bail!("query cannot be longer than {MAX_QUERY_LEN} characters")
     }
     Ok(())
+}
+
+fn get_domain(q: &str) -> String {
+    let parts: Vec<&str> = q.split('.').collect();
+    parts
+        .windows(2)
+        .find(|w| TLDS.contains(w[1]))
+        .map(|w| format!("{}.{}", w[0], w[1]))
+        .unwrap_or_else(|| format!("{}.{}", parts[0], DEFAULT_DOMAIN))
 }
