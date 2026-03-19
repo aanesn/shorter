3631a27cfe508a2471a967e8f428cc6c2264f771
aanesn <anesen.niklas@gmail.com>
2026-03-19 09:01:00 +0100

    anyhow error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use axum::{Router, routing::get};
 
+mod error;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
-        .await
-        .unwrap();
-    println!("listening on http://{}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+    println!("listening on http://{}", listener.local_addr()?);
+    Ok(axum::serve(listener, app).await?)
 }
