0ed3ecf6a41e61ccdf4e0d8df21a646c7fb49787
aanesn <anesen.niklas@gmail.com>
2026-04-15 08:27:59 +0200

    link

---
+++ web/src/lib/components/Link.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { cva, cx } from "$lib/utils"
+	import type { VariantProps } from "cva"
+	import type { HTMLAnchorAttributes } from "svelte/elements"
+
+	const link = cva({
+		base: "inline-flex shrink-0 items-center justify-center rounded-full text-sm whitespace-nowrap transition-all outline-none select-none [&_svg]:pointer-events-none [&_svg]:shrink-0",
+		variants: {
+			intent: {
+				primary: "bg-white text-black hover:bg-white/80",
+				secondary: "hover:bg-neutral-900"
+			},
+			size: {
+				default: "h-7 px-2.5"
+			}
+		}
+	})
+
+	let {
+		intent = "primary",
+		size = "default",
+		class: className,
+		children,
+		...others
+	}: HTMLAnchorAttributes & {
+		intent?: VariantProps<typeof link>["intent"]
+		size?: VariantProps<typeof link>["size"]
+	} = $props()
+</script>
+
+<a class={cx(link({ intent, size }), className)} {...others}>
+	{@render children?.()}
+</a>
--- web/src/routes/components/Header.svelte
+++ web/src/routes/components/Header.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import Logomark from "$lib/assets/logomark.svg?raw"
+	import Link from "$lib/components/Link.svelte"
+
+	const nav = [
+		{ title: "dynadot", href: "https://www.dynadot.com/domain/search?rscreg=shorter" },
+		{ title: "repo", href: "https://github.com/aanesn/shorter" },
+		{ title: "contact", href: "mailto:contact@shorter.dev" }
+	]
 </script>
 
 <header class="flex h-16 items-center justify-between">
-	<a href="/">
-		{@html Logomark}
-	</a>
+	<nav class="flex items-center gap-x-2">
+		<a href="/" class="mr-2.5">
+			{@html Logomark}
+		</a>
+		{#each nav as { title, href }}
+			<Link {href} intent="secondary" class="hidden lg:flex">
+				{title}
+			</Link>
+		{/each}
+	</nav>
+	<Link class="duration-300">start now</Link>
 </header>
