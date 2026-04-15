ac1818801089f36d001691fc01defd47bf3f32a0
aanesn <anesen.niklas@gmail.com>
2026-04-15 07:19:25 +0200

    logomark

---
+++ web/src/routes/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Logomark from "$lib/assets/logomark.svg?raw"
+</script>
+
+<header class="flex h-16 items-center justify-between">
+	<a href="/">
+		{@html Logomark}
+	</a>
+</header>
