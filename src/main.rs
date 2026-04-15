4f0205850837b8d8cc2625e9818b8562b930d7de
aanesn <anesen.niklas@gmail.com>
2026-04-15 06:33:01 +0200

    cors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{Router, routing::get};
+use axum::{Router, http::Method, routing::get};
+use tower_http::cors::CorsLayer;
 
 mod error;
 mod search;
@@ -6,9 +7,17 @@ mod tlds;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin([
+            "https://shorter.dev".parse()?,
+            "http://localhost:5173".parse()?,
+        ]);
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/search", get(search::get));
+        .route("/search", get(search::get))
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
     println!("listening on http://{}", listener.local_addr()?);
