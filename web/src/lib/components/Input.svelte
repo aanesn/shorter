6bc06ef3cf54a552175735a92dbbc12dc86c42fd
aanesn <anesen.niklas@gmail.com>
2026-04-15 11:08:21 +0200

    input + small h1 fix

---
+++ web/src/lib/components/Input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from "svelte/elements"
+	import { cx } from "$lib/utils"
+
+	let { value = $bindable(), class: className, ...others }: HTMLInputAttributes = $props()
+</script>
+
+<input
+	class={cx(
+		"h-12 w-full min-w-0 rounded-full bg-neutral-900 px-3.5 py-1 shadow-xs transition-[color,box-shadow] outline-none placeholder:text-neutral-500",
+		className
+	)}
+	bind:value
+	{...others}
+/>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Input from "$lib/components/Input.svelte"
 	import Header from "./components/Header.svelte"
 </script>
 
@@ -10,7 +11,8 @@
 <Header />
 
 <div class="py-40">
-	<h1 class="text-center text-3xl font-medium lg:text-4xl">
-		find shorter versions of your domain.
-	</h1>
+	<div class="flex flex-col items-center gap-y-6">
+		<h1 class="text-center text-3xl font-medium">find shorter versions of your domain.</h1>
+		<Input class="max-w-xl" placeholder="type a domain..." autocomplete="off" autofocus />
+	</div>
 </div>
