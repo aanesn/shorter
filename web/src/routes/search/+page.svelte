3a42a573e283ea9fc24131e0974d8d12a89f3ebd
aanesn <anesen.niklas@gmail.com>
2026-04-16 02:16:18 +0200

    add browser checks

--- web/src/routes/search/+page.svelte
+++ web/src/routes/search/+page.svelte
@@ -1,11 +1,13 @@
 <script>
+	import { browser } from "$app/environment"
 	import { goto } from "$app/navigation"
 	import { page } from "$app/state"
 	import Input from "$lib/components/Input.svelte"
 
-	let value = $state(page.url.searchParams.get("q") ?? "")
+	let value = $state(browser ? (page.url.searchParams.get("q") ?? "") : "")
 
 	$effect(() => {
+		if (!browser) return
 		goto(value ? "/search?q=" + encodeURIComponent(value) : "/search", { replaceState: true })
 	})
 </script>
