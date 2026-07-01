3a240033c2ceaa385790cd827cafee4f7746ca86
aanesn <anesen.niklas@gmail.com>
2026-07-01 03:11:29 +0200

    404

---
+++ web/src/routes/404/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>404 | shorter</title>
+	<meta name="description" content="page not found" />
+</svelte:head>
+
+<div class="flex min-h-screen items-center justify-center">
+	<h1 class="text-lg">404: page not found</h1>
+</div>
